133fdf1958d89ca8b1c4cb8bc5698fcd5fc503ea
usamaa1 <usamariz1997@gmail.com>
2025-04-29 20:09:51 +0500

    2209G2 till delete work done

--- public/React_2209G2/projecyt/src/Post/createPost.jsx
+++ public/React_2209G2/projecyt/src/Post/createPost.jsx
@@ -1,7 +1,6 @@
-
-import { Container, Form } from 'react-bootstrap';
+import { Container, Form, Card, Button } from 'react-bootstrap';
 import axios from 'axios';
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 
 function CreatePost() {
@@ -21,15 +20,58 @@ function CreatePost() {
                 likes: likesFeild.current.value,
 
             },
-        )
-        console.log(data)
+            )
+            console.log(data)
 
-
+            postGet();
 
         } catch (e) {
             console.log(e);
         }
     }
+
+    let [postArray, setPostArray] = useState([]);
+
+
+    const postGet = async (e) => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/v1/post');
+            console.log(response);
+            const { data } = response;
+
+            setPostArray(data);
+
+            console.log(data)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    const postDelete = async (delId)=>{
+        try {
+
+
+            if(confirm("Are you sure to Delete this?")){
+                const response = await axios.delete(`http://localhost:3000/api/v1/post/${delId}`);
+                console.log(response);
+                postGet();
+            }
+
+         
+         
+
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+
+    useEffect(() => {
+        postGet();
+    },[])
 
 
 
@@ -54,6 +96,28 @@ function CreatePost() {
                 </Form.Group>
                 <button type='submit' className='btn btn-primary'>Submit</button>
             </Form>
+        </Container>
+
+
+        <Container>
+
+
+            {postArray.map((singlePost) => (
+
+                <Card key={singlePost._id}>
+                    <Card.Header as="h5">{singlePost.title}</Card.Header>
+                    <Card.Body>
+                        <Card.Title>{singlePost.description}</Card.Title>
+                        <Card.Text>
+                           {singlePost.likes}
+                        </Card.Text>
+                        <Button variant="danger" onClick={()=>postDelete(singlePost._id)}>Delete</Button>
+                    </Card.Body>
+                </Card>
+
+            ))}
+
+
         </Container>
 
     </>
