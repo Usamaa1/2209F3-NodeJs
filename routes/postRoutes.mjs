cee3aeb02d17cd2115b992fbeb1846ea46801553
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2025-07-09 23:31:47 +0500

    Delete routes directory

--- routes/postRoutes.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPost, filterPostsByDate } from "../controllers/postController.mjs";
-
-const router = express.Router();
-
-router.post("/post/createPost", createPost);
-router.get("/post/postDateFilter", filterPostsByDate);
-
-export default router;
